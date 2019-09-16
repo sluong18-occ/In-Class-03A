2235622f105be649bedaf460b395ecef2060f80d
sluong18-occ <54555938+sluong18-occ@users.noreply.github.com>
2019-09-16 13:12:18 -0700

    Create CallingCard.java

---
+++ CallingCard.java
@@ -0,0 +1,13 @@
+package ic03a.question1;
+
+public class CallingCard extends Card
+{
+  private String cardNumber;
+  private String pin;
+  public CallingCard(String n, String cardNumber, String pin)
+  {
+    super(n);
+    this.cardNumber = cardNumber;
+    this.pin = pin;
+  }
+}
