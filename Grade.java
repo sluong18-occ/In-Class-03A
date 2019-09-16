28188328ee07c0008d152824ab91a967f4c871fe
sluong18-occ <54555938+sluong18-occ@users.noreply.github.com>
2019-09-16 13:06:54 -0700

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,32 @@
+package occ.cs272.h01;
+
+import java.util.Scanner;
+
+public class Grade
+{
+  public statidc final String STUDENT = "Put your OCC login name here";
+  public static final String ASSIGNMENT = "H01-C";
+  
+  private String grade;
+  
+  public Grade(String input)
+  {
+    grade = input;
+  {
+  
+  public double getNumericgrade()
+  {
+    return 10;
+  }
+  public static void main(String[] args)
+  {
+    try (Scanner in = new Scanner(System.in))
+    {
+      System.out.print("Enter a letter grade: ");
+      String input = in.nextLine();
+      Grade g = new Grade(input);
+      double grade = g.getNumericGrade();
+      System.out.println("Numeric value:  " + grade);
+    }
+  }
+}
